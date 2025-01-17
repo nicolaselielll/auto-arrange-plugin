0b9be7a05606e08584d08349d054ec3e4f5fee14
nicolastuomaala <nicolas.tuomaala00@gmail.com>
2025-01-18 00:38:38 +0200

    asd

---
+++ _types/presets/classic/index.d.ts
@@ -0,0 +1,12 @@
+import { Preset } from '../types';
+/**
+ * Classic preset. Input ports are positioned on the left bottom side of the node, output ports are positioned on the right top side of the node.
+ * @priority 7
+ */
+export declare const setup: (props?: {
+    spacing?: number;
+    top?: number;
+    bottom?: number;
+    x?: x;
+}) => Preset;
+//# sourceMappingURL=index.d.ts.map
--- src/presets/classic/index.ts
+++ src/presets/classic/index.ts
@@ -1,40 +1,31 @@
 import { PortData, Preset } from '../types'
 
-/**
- * Classic preset. Input ports are positioned on the left bottom side of the node, output ports are positioned on the right top side of the node.
- * @priority 7
- */
 export const setup = (props?: { spacing?: number, top?: number, bottom?: number, x?: number }): Preset => {
   return () => ({
     port(data): PortData {
-      const { spacing, top, bottom } = {
-        spacing: typeof props?.spacing !== 'undefined'
-          ? props.spacing
-          : 35,
-        top: typeof props?.top !== 'undefined'
-          ? props.top
-          : 35,
-        bottom: typeof props?.bottom !== 'undefined'
-          ? props.bottom
-          : 15
-      }
+      const { spacing, top, bottom, x } = {
+        spacing: typeof props?.spacing !== 'undefined' ? props.spacing : 35,
+        top: typeof props?.top !== 'undefined' ? props.top : 35,
+        bottom: typeof props?.bottom !== 'undefined' ? props.bottom : 15,
+        x: typeof props?.x !== 'undefined' ? props.x : 0 // Default value for x
+      };
 
       if (data.side === 'output') {
         return {
-          x: props.x,
+          x: x, // Use the destructured x directly
           y: top + data.index * spacing,
           width: 15,
           height: 15,
           side: 'EAST'
-        }
+        };
       }
       return {
-        x: -props.x,
+        x: -x, // Use the destructured x directly
         y: data.height - bottom - data.ports * spacing + data.index * spacing,
         width: 15,
         height: 15,
         side: 'WEST'
-      }
+      };
     }
-  })
-}
+  });
+};
